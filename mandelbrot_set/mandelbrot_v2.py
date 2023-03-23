c014d48f8c052e379f9a30649a47ec024fbcc05a
n.vdovin <narmunine@gmail.com>
2023-03-23 23:48:24 +0300

    It start to working! Or not? Now I had a circle of Zhuliass set...

--- mandelbrot_set/mandelbrot_v2.py
+++ mandelbrot_set/mandelbrot_v2.py
@@ -1,14 +1,15 @@
 from utils import *
 import numpy as np
 import pygame as pg
+import time
 
 
 def get_Mandelbrods_set(max_iterations=10):
     accept = got_out = []
     
-    for x, x_point in enumerate(x_coordinates):
-        for y, y_point in enumerate(y_coordinates):
-            c = x_point + y_point * 1j
+    for x in x_coordinates:
+        for y in y_coordinates:
+            c = x + y * 1j
             z = 0
 
             for iteration in range(max_iterations):
@@ -18,15 +19,22 @@ def get_Mandelbrods_set(max_iterations=10):
                     if iteration == 0:
                         color = 255
                     else:
-                        sharpness = 255 / iteration
-                        color = round(number)((max_iterations / iteration) * sharpness)
+                        sharpness = 255 / max_iterations
+                        color = round((max_iterations / iteration) * sharpness)
 
-                    return x_point, y_point, color
-
+                    x_coord, y_coord = round((x * zoom) + x_center), round((y * zoom) + y_center)
+                    pg.draw.circle(sc, (color, color, color), (x_coord, y_coord), 2)
+                    print(f"x: {x}, y: {y} Вылетел на {iteration}, цвет: {color}")
+                    break
                 else:
-                    color = (0, 0, 0)
-                    return (x_point, y_point, color)
+                    color = 0
 
+                    x_coord, y_coord = round((x * zoom) + x_center), round((y * zoom) + y_center)
+                    print(f"x: {x}, y: {y} Удовлетворяет, цвет: {color}")
+                    pg.draw.circle(sc, (color, color, color), (x_coord, y_coord), 2)
+                    break
+
+                    
 
 x_coordinates = y_coordinates = np.linspace(-2, 2, 30)
 
@@ -34,19 +42,16 @@ pg.init()
 sc = pg.display.set_mode((WIGHT, HEIGHT))
 pg.display.set_caption("Mandelbrod`s set")
 
-zoom = 10
+zoom = 50
 
+sc.fill((5, 10, 7))
+
+get_Mandelbrods_set()
+
+pg.display.update()
 cycle = True
 while cycle:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             cycle = False
-
-    sc.fill((5, 10, 7))
-
-    x, y, color = get_Mandelbrods_set()
-    x, y = x + round((x_center) * zoom), round((y + y_center) * zoom)
-
-    pg.draw.circle(sc, (color, color, color), (x, y), 2)
-
-    pg.display.update()
+            
