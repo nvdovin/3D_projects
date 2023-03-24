c18cfa6f97ae3535d43a6c9d389698a9ea6dca2c
n.vdovin <narmunine@gmail.com>
2023-03-24 12:24:28 +0300

    I saw it! Yea!

--- mandelbrot_set/mandelbrot_v2.py
+++ mandelbrot_set/mandelbrot_v2.py
@@ -1,7 +1,6 @@
 from utils import *
 import numpy as np
 import pygame as pg
-import time
 
 
 def get_Mandelbrods_set(max_iterations=10):
@@ -9,44 +8,37 @@ def get_Mandelbrods_set(max_iterations=10):
     
     for x in x_coordinates:
         for y in y_coordinates:
-            c = x + y * 1j
-            z = 0
+            c = complex(x, y)
+            z = complex(0)
 
             for iteration in range(max_iterations):
                 z = z ** 2 + c
 
                 if abs(z) > 2:
                     if iteration == 0:
-                        color = 255
+                        R = G = B = 0
                     else:
-                        sharpness = 255 / max_iterations
-                        color = round((max_iterations / iteration) * sharpness)
+                        R = abs(round(max_iterations / iteration) - 100)
+                        G = abs(round( max_iterations / iteration) - 70)
+                        B = round(max_iterations / iteration)
 
                     x_coord, y_coord = round((x * zoom) + x_center), round((y * zoom) + y_center)
-                    pg.draw.circle(sc, (color, color, color), (x_coord, y_coord), 2)
-                    print(f"x: {x}, y: {y} Вылетел на {iteration}, цвет: {color}")
-                    break
-                else:
-                    color = 0
-
-                    x_coord, y_coord = round((x * zoom) + x_center), round((y * zoom) + y_center)
-                    print(f"x: {x}, y: {y} Удовлетворяет, цвет: {color}")
-                    pg.draw.circle(sc, (color, color, color), (x_coord, y_coord), 2)
+                    pg.draw.circle(sc, (R, G, B), (x_coord, y_coord), 1)
+                    print(f"x: {x}, y: {y} Вылетел на {iteration}, цвет: {R, G, B}. Z = {z}")
                     break
 
                     
 
-x_coordinates = y_coordinates = np.linspace(-2, 2, 30)
+x_coordinates = y_coordinates = np.linspace(-2, 2, 200)
 
 pg.init()
 sc = pg.display.set_mode((WIGHT, HEIGHT))
 pg.display.set_caption("Mandelbrod`s set")
 
-zoom = 50
-
+zoom = 180
 sc.fill((5, 10, 7))
 
-get_Mandelbrods_set()
+get_Mandelbrods_set(255)
 
 pg.display.update()
 cycle = True
