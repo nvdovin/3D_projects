844e4e5adc7b931bba2668599cceedbebdf4a9df
n.vdovin <narmunine@gmail.com>
2023-03-26 18:52:30 +0300

    added utils

---
+++ mandelbrot_set/utils.py
@@ -0,0 +1,12 @@
+# Configurations
+
+WIGHT = 1080
+HEIGHT = round((9 / 16) * WIGHT)
+h = WIGHT / HEIGHT
+
+x_center = (WIGHT // 2) + (WIGHT // 5)
+y_center = HEIGHT // 2
+
+# Colors
+
+BLACK = (10, 7, 12)
